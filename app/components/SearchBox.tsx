8edbc124c7a54262368466faf4e61e6a3375e3cc
Taylor Clark <taylorstevenclark@gmail.com>
2023-07-08 14:00:38 -0500

    init commit

---
+++ app/components/SearchBox.tsx
@@ -0,0 +1,68 @@
+"use client";
+import { match } from "assert";
+import React from "react";
+import cities from "../lib/city.list.json";
+import Link from "next/link";
+
+export default function SearchBox({ placeholder }) {
+  const [query, setQuery] = React.useState("");
+  const [results, setResults] = React.useState([]);
+
+  const onChange = (e) => {
+    const { value } = e.target;
+    setQuery(value);
+
+    let matchingCities = [];
+
+    if (value.length > 3) {
+      for (let city of cities) {
+        if (matchingCities.length >= 5) {
+          break;
+        }
+
+        const match = city.name.toLowerCase().startsWith(value.toLowerCase());
+
+        if (match) {
+          const cityData = {
+            ...city,
+            slug: `${city.name.toLowerCase().replace(/ /g, "-")}-${city.id}`,
+          };
+          matchingCities.push(cityData);
+        }
+      }
+    }
+
+    return setResults(matchingCities);
+  };
+
+  return (
+    <div className="search">
+      <input
+        type="text"
+        value={query}
+        onChange={onChange}
+        placeholder={placeholder ? placeholder : ""}
+      />
+
+      {query.length > 3 && (
+        <ul>
+          {results.length > 0 ? (
+            results.map((city) => {
+              return (
+                <li key={city.slug}>
+                  <Link href={`/location/${city.slug}`}>
+                    {city.name}
+                    {city.state ? `, ${city.state}` : ""}{" "}
+                    <span>({city.country})</span>
+                  </Link>
+                </li>
+              );
+            })
+          ) : (
+            <li className="search__no-results">No results found</li>
+          )}
+        </ul>
+      )}
+    </div>
+  );
+}
---
+++ pages/location/[city].tsx
@@ -0,0 +1,103 @@
+import React from "react";
+import "../../styles/main.scss";
+import cities from "../../app/lib/city.list.json";
+import Link from "next/link";
+import Head from "next/head";
+import SearchBox from "../../app/components/SearchBox";
+
+export async function getServerSideProps(context) {
+  const city = getCity(context.params.city);
+  if (!city) {
+    return {
+      notFound: true,
+    };
+  }
+
+  const res = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${city.coord.lat}&lon=${city.coord.lon}&appid=${process.env.API_KEY}&units=imperial&exclude=minutely`
+  );
+
+  const data = await res.json();
+
+  if (!data) {
+    return {
+      notFound: true,
+    };
+  }
+
+  console.log(data);
+
+  const slug = context.params.city;
+  return {
+    props: {
+      city: city,
+      data: data,
+      currentTemp: data.main.temp,
+    },
+  };
+}
+
+const getCity = (param) => {
+  const cityParam = param.trim();
+  // get city id
+  const splitCity = cityParam.split("-");
+  const id = splitCity[splitCity.length - 1];
+
+  if (!id) {
+    return null;
+  }
+
+  const city = cities.find((city) => city.id.toString() == id);
+
+  if (city) {
+    return city;
+  } else {
+    return null;
+  }
+};
+
+export default function City({ slug, data }) {
+  let currentTemp = Math.round(data.main.temp);
+  let currentLow = Math.round(data.main.temp_min);
+  let currentHigh = Math.round(data.main.temp_max);
+  let feelsLike = Math.round(data.main.feels_like);
+  let wind = Math.round(data.wind.speed);
+
+  let cityName = data.name;
+
+  return (
+    <>
+      <Head>
+        <title>Weather - Next Weather App</title>
+      </Head>
+
+      <div className="container">
+        <div className="cityName">{cityName}</div>
+        <div className="middleRow">
+          <div className="currentTemp">{currentTemp}&deg;F</div>
+          <div className="wind">
+            <span className="windText">{wind}</span>
+            <span class="largeLabel">MPH</span>{" "}
+            <div class="smallLabel">winds</div>
+          </div>
+        </div>
+        <div className="bottomRow">
+          <div className="feelsLike">
+            <div class="smallLabel">Feels Like</div>
+            {feelsLike}&deg;F
+          </div>
+          <div className="currentHigh">
+            <div class="smallLabel">High</div>
+            {currentHigh}&deg;F
+          </div>
+          <div className="currentLow">
+            <div class="smallLabel">Low</div>
+            {currentLow}&deg;F{" "}
+          </div>
+        </div>
+
+        <SearchBox placeholder="Search for a city..." />
+      </div>
+    </>
+  );
+}
